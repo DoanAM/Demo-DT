01325ba9421d65b6ffc0e6aa28b076f0587103b1
Your Name <augustino95@yahoo.de>
2023-03-10 23:19:31 +0100

    color changer

--- frontend/src/components/Line.jsx
+++ frontend/src/components/Line.jsx
@@ -7,12 +7,13 @@ const Line = (props) => {
   //points.push(new THREE.Vector3(-10000, 0, 0));
   //points.push(new THREE.Vector3(0, 10000, 0));
   //points.push(new THREE.Vector3(10000, 0, 0));
+
   const lineGeometry = new THREE.BufferGeometry().setFromPoints(props.points);
   return (
     <line geometry={lineGeometry}>
       <lineBasicMaterial
         attach="material"
-        color={props.color}
+        color={props.color[props.displayColor]}
         linewidth={1}
         linecap={"round"}
         linejoin={"round"}
