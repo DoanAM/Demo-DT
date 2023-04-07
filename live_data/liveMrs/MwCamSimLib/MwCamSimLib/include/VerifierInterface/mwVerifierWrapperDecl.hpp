d38ce5de119fa55ee0d2d1e301fecf964bdc310e
Your Name <augustino95@yahoo.de>
2023-04-07 16:59:38 +0200

    Remove Live-MRS

--- live_data/liveMrs/MwCamSimLib/MwCamSimLib/include/VerifierInterface/mwVerifierWrapperDecl.hpp
+++
@@ -1,8 +0,0 @@
-// (C) 2014-2015 by ModuleWorks GmbH
-// Owner: CutSim
-
-#pragma once
-// DEPRECATED: please don't include this header
-#pragma message("Warning: please don't include " __FILE__ ", it is deprecated and will be removed")
-
-#include "mwVerifierWrapper.hpp"
--- live_data/liveMrs/MwCamSimLib/MwCamSimLib/include/VerifierInterface/mwvMeasureResults.hpp
+++
@@ -1,6 +0,0 @@
-// (C) 2020-2021 by ModuleWorks GmbH
-// Owner: CutSim
-
-#pragma message("Warning: " __FILE__ \
-				" is deprecated, please include mwvMeasureDimensions.hpp instead!")
-#include "mwvMeasureDimensions.hpp"
